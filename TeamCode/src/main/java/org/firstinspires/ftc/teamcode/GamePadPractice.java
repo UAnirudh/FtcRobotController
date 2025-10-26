b17eb71beef087eeb576bd38a42a8f1d2eea8d8d
UAnirudh <uanirudh0811@gmail.com>
2025-10-25 17:02:42 -0700

    Created Game Pad Practice
    I learned to use Gamepad

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadPractice.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@Disabled
+@TeleOp
+public class GamePadPractice extends OpMode {
+    public void init(){
+
+    }
+    public void loop(){
+        // runs 50x a second
+        double triggerSum = gamepad1.left_trigger+gamepad1.right_trigger;
+        double speedForward = -gamepad1.left_stick_y/2.0;
+        double differenceOfx = gamepad1.left_stick_x-gamepad1.right_stick_x;
+        telemetry.addData("x",gamepad1.left_stick_x);
+        telemetry.addData("y",speedForward);
+        telemetry.addData("a button",gamepad1.a);
+        telemetry.addData("X",gamepad1.right_stick_x);
+        telemetry.addData("Y",gamepad1.right_stick_y);
+        telemetry.addData("b button",gamepad1.b);
+        telemetry.addData("Difference of 'x's",differenceOfx);
+        telemetry.addData("Sum of Triggeres",triggerSum);
+
+
+
+
+
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables_Examples.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Variables_Examples {
+    //Data Types and Init
+    int robotHeight;
+    double motorSpeed;
+    boolean claw_state;
+    /*
+    robotHeight = 20;
+    motorSpeed = 12.3;
+    state = true;
+    */
+
+}
